f659ea1e1b54fa429dcde34f8b2c1c1e89debf24
ranjankumar <ranjankumar@123gmail.com>
2023-10-21 17:47:39 +0530

    first commit

---
+++ LAMP-Application/php-Apache2-certbot-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update package repositories
+sudo apt update -y
+
+# Install PHP and required extensions
+sudo apt install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath -y
+
+# Display PHP version
+php -v
+
+echo "PHP installation and setup complete."
+
+sudo apt install apache2 -y
+
+apache2 -v 
+
+echo "Apache installation and setup complete."
+
+# certbot installation
+
+sudo apt install certbot python3-certbot-apache -y
+
